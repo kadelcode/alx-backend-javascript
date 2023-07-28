50703f694dafa8daf5e9d8e5c5cd28497066fe7d
Ayodele Kadiri <kadelcode@gmail.com>
2023-07-28 19:50:00 +0000

    code fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,12 @@
+/* eslint-disable import/extensions */
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup () {
-  return Promise
-    .all([uploadPhoto(), createUser()])
-    .then((res) => {
-      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
-    })
-    .catch(() => console.log('Signup system offline'));
+export default function handleProfileSignup() {
+  const upload = uploadPhoto();
+  const create = createUser();
+  return Promise.all([upload, create]).then((result) => {
+    console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
+  }).catch(() => {
+    console.log('Signup system offline');
+  });
 }
