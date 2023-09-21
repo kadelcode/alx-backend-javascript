d683787061d924a868a2beea1396782ec771bdf9
Ayodele Kadiri <kadelcode@gmail.com>
2023-09-21 19:03:34 +0000

    payment test

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,20 +1,28 @@
 const sinon = require('sinon');
-const Utils = require('./utils');
 const { expect } = require('chai');
-const sendPaymentRequestToApi = require('./4-payment');
+const sendPaymentRequestToApi = require('./5-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('sendPaymentRequestToApi using calculateNumber form Utils', () => {
-   const stubSpy = sinon.stub(Utils, 'calculateNumber');
-   const sinoSpy = sinon.spy(console);
-
-   stubSpy.returns(10);
-   sendPaymentRequestToApi(100, 20);
-   expect(stubSpy.calledWith('SUM', 100, 20)).to.be.true;
-   expect(stubSpy.callCount).to.be.equal(1);
-   expect(sinoSpy.log.calledWith('The total is: 10')).to.be.true;
-   expect(sinoSpy.log.callCount).to.be.equal(1);
-   stubSpy.restore();
-   sinoSpy.log.restore();
+  let sinoSpy;
+  beforeEach(() => {
+    if(!sinoSpy) {
+     sinoSpy = sinon.spy(console);
+    }
   });
+  afterEach(() => {
+   sinoSpy.log.resetHistory();
+  });
+
+  it('sendPaymentRequestToApi(100, 20) should log "The total is: 120"', () => {
+   sendPaymentRequestToApi(100, 20);
+   expect(sinoSpy.log.calledWith('The total is: 120')).to.be.true;
+   expect(sinoSpy.log.calledOnce).to.be.true;
+  });
+
+ it('sendPaymentRequestToApi(10, 10) should log "The total is: 20"', () => {
+   sendPaymentRequestToApi(10, 10);
+   expect(sinoSpy.log.calledWith('The total is: 20')).to.be.true;
+   expect(sinoSpy.log.calledOnce).to.be.true;
+  });
+
 });
